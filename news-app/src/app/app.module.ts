13bfa55603efc1c0bc5de17382cfdaafe42b576a
Filip Filipovic <filipfilipovic.stray@gmail.com>
2019-09-13 16:18:33 +0200

    added components, routes and styles for top-news

--- news-app/src/app/app.module.ts
+++ news-app/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TopNewsComponent } from './top-news/top-news.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CategoriesComponent } from './categories/categories.component';
+import { SearchComponent } from './search/search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopNewsComponent,
+    CategoriesComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
